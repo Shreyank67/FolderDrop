76ad17dd755463dcff4956da73386bdf0844a8f0
Shreyank Patil <shreyankpatil7@gmail.com>
2026-06-25 22:32:34 +0530

    Convert app to menu bar application

--- FolderDrop/FolderDropApp.swift
+++ FolderDrop/FolderDropApp.swift
@@ -10,8 +10,9 @@ import SwiftUI
 @main
 struct FolderDropApp: App {
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra("FolderDrop", systemImage: "folder") {
             ContentView()
         }
+        .menuBarExtraStyle(.window)
     }
 }
