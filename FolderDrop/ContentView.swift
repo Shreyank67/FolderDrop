8f969cfbfdf326bcd7d5b9dfba6ddc1a1142aa05
Shreyank Patil <shreyankpatil7@gmail.com>
2026-06-25 21:34:16 +0530

    Initial Commit

---
+++ FolderDrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FolderDrop
+//
+//  Created by Shreyank Patil on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FolderDrop/FolderDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderDropApp.swift
+//  FolderDrop
+//
+//  Created by Shreyank Patil on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FolderDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
